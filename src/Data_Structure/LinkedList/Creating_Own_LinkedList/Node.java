da92110c200158afcba1b8e6bee934343fca273c
ashmitsharma1706 <0126cd211030@oriental.ac.in>
2024-05-01 10:31:15 +0530

    LinkedList

---
+++ src/Data_Structure/LinkedList/Creating_Own_LinkedList/Node.java
@@ -0,0 +1,6 @@
+package Data_Structure.LinkedList.Creating_Own_LinkedList;
+
+public class Node {
+    int data;
+    Node next;
+}
---
+++ src/Data_Structure/LinkedList/Creating_Own_LinkedList/UseNode.java
@@ -0,0 +1,27 @@
+package Data_Structure.LinkedList.Creating_Own_LinkedList;
+
+public class UseNode {
+    public static void main(String[] args) {
+        Node head;
+        head=new Node();
+        head.data=10;
+
+        Node sec=new Node();
+        sec.data=20;
+        head.next=sec;
+
+        Node third=new Node();
+        third.data=30;
+        sec.next=third;
+
+        Node fourth=new Node();
+        fourth.data=40;
+        third.next=fourth;
+
+        Node p=head;
+        while (p!=null){
+            System.out.println(p.data);
+            p=p.next;
+        }
+    }
+}
