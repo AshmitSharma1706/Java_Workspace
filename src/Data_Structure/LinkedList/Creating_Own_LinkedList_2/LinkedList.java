4dd6f778c1b36770ed3563452ac98a13f2d2312f
ashmitsharma1706 <0126cd211030@oriental.ac.in>
2024-05-07 14:52:29 +0530

    LinkedList_2 methods added

--- src/Data_Structure/LinkedList/Creating_Own_LinkedList_2/LinkedList.java
+++ src/Data_Structure/LinkedList/Creating_Own_LinkedList_2/LinkedList.java
@@ -1,5 +1,7 @@
 package Data_Structure.LinkedList.Creating_Own_LinkedList_2;
 
+import java.util.Stack;
+
 public class LinkedList {
     private Node head;
 
@@ -48,5 +50,66 @@ public class LinkedList {
             }
         }
         return position;
+    }
+    public void prepend(int x){
+        Node p=new Node(x);
+        p.setNext(head);
+        head=p;
+    }
+    public void printReverse(){
+        Stack<Integer> s = new Stack<>();
+        Node temp=head;
+        while (temp!=null){
+            int x= temp.getData();
+            s.push(x);
+            temp=temp.getNext();
+        }
+        while (!s.empty()){
+            int x=s.pop();
+            System.out.println(x);
+        }
+    }
+    public void addAt(int at,int x){
+        if(at<0 || at>count()){
+            throw new IllegalArgumentException("Invalid position..!");
+        }
+        Node p=new Node(x);
+        if(at==0){
+            p.setNext(head);
+            head=p;
+            return;
+        }
+        Node temp=head;
+        int i=0;
+        while (i<at-1){
+            i++;
+            temp=temp.getNext();
+        }
+        p.setNext(temp.getNext());
+        temp.setNext(p);
+    }
+    public void insert(int x){
+        Node p = new Node(x);
+        if (head==null){
+            head=p;
+            return;
+        }
+        if(x< head.getData()){
+            p.setNext(head);
+            head=p;
+            return;
+        }
+        Node temp=head,prev=null;
+        while (temp!=null && temp.getData()<x){
+            prev=temp;
+            temp=temp.getNext();
+        }
+        if(temp==null){
+            prev.setNext(p);
+        }
+        else {
+            prev.setNext(p);
+            p.setNext(temp);
+        }
     }
 }
