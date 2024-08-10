71662d2f672158675abbca197abdc2b8da00442c
ashmitsharma1706 <0126cd211030@oriental.ac.in>
2024-08-10 20:01:10 +0530

    Factorial with Recursion...!

--- src/Data_Structure/Recursion/Recursion3.java
+++ src/Data_Structure/Recursion/Recursion3.java
@@ -2,7 +2,8 @@ package Data_Structure.Recursion;
 
 public class Recursion3 {
     public static void main(String[] args) {
-        greetings(5);
+//        greetings(5);
+        System.out.println(factorial(3));
     }
     public static void greetings(int n){
         if(n>0){
@@ -10,5 +11,11 @@ public class Recursion3 {
             greetings(n-1);
             System.out.println("Bye..!");
         }
+    }
+    public static int factorial(int n){
+        if(n<2){
+            return 1;
+        }
+        return n * factorial(n-1);
     }
 }
