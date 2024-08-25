725e4ec19ea8eea728bc190785886b7d30e1f1e0
ashmitsharma1706 <0126cd211030@oriental.ac.in>
2024-08-25 19:29:50 +0530

    Practicing...!

---
+++ src/Data_Structure_2/Maths/PrimesTillNumber.java
@@ -0,0 +1,23 @@
+package Data_Structure_2.Maths;
+
+public class PrimesTillNumber {
+    public static void main(String[] args) {
+        int n=40;
+        boolean[] primes=new boolean[n+1];
+        isPrime(n,primes);
+    }
+    public static void isPrime(int n, boolean[] primes){
+        for (int i = 2; i*i <= n; i++) {
+            if(!primes[i]){
+                for (int j = i*2; j <= n; j+=i) {
+                    primes[j]=true;
+                }
+            }
+        }
+        for (int i = 2; i <= n; i++) {
+            if(!primes[i]){
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
